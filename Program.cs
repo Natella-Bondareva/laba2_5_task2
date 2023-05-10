b0ac38f5b9e660466feabccfddff5484c4a08ac3
natella <natellabondareva20@gmail.com>
2023-05-10 07:07:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace laba2_5_task2
+{
+    internal class Program
+    {
+        static Student[] ReadData(string fileName)
+        // You may (if want) change ``Student[]'' to ``List<Student>''
+        {
+            // TODO   implement this method.
+            // It should read the file whose fileName has been passed and fill data inside array of Student
+        }
+
+        static void runMenu(Student[] studs)
+        // You may (if want) change ``Student[]'' to ``List<Student>''
+        {
+            // TODO   implement this method
+            // It should call method(s) for specific variant(s).
+            // Array (or List) studs should already contain data (probably filled with ReadData method)
+        }
+        static void Main(string[] args)
+        {
+            Student[] studs = ReadData("input.txt");
+            runMenu(studs);
+        }
+    }
+}
